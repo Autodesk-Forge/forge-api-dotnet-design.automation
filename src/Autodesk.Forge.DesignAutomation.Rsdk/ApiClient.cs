e7ccdb3a6574a5dbdcbce2ef8d1363ba0727b31a
Albert Szilvasy <albert.szilvasy@autodesk.com>
2019-01-19 08:09:14 -0800

    Adding copyright headers

--- src/Autodesk.Forge.DesignAutomation.Rsdk/ApiClient.cs
+++ src/Autodesk.Forge.DesignAutomation.Rsdk/ApiClient.cs
@@ -1,5 +1,24 @@
-﻿using Autodesk.Forge.DesignAutomation.Model;
-using Autodesk.Forge.DesignAutomation.Http;
+﻿/* 
+ * Forge SDK
+ *
+ * The Forge Platform contains an expanding collection of web service components that can be used with Autodesk cloud-based products or your own technologies. Take advantage of Autodesk’s expertise in design and engineering.
+ *
+ * Design Automation
+ *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using Autodesk.Forge.DesignAutomation.Rsdk.Model;
+using Autodesk.Forge.DesignAutomation.Rsdk.Http;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.IO;
@@ -7,7 +26,7 @@ using System.Collections.Generic;
 using System;
 using Autodesk.Forge.Core;
 
-namespace Autodesk.Forge.DesignAutomation
+namespace Autodesk.Forge.DesignAutomation.Rsdk
 {
     public partial class DesignAutomationClient
     {
--- src/Autodesk.Forge.DesignAutomation.Rsdk/Model/SettingConverter.cs
+++ src/Autodesk.Forge.DesignAutomation.Rsdk/Model/SettingConverter.cs
@@ -1,9 +1,28 @@
-﻿using Newtonsoft.Json;
+﻿/* 
+ * Forge SDK
+ *
+ * The Forge Platform contains an expanding collection of web service components that can be used with Autodesk cloud-based products or your own technologies. Take advantage of Autodesk’s expertise in design and engineering.
+ *
+ * Design Automation
+ *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using System;
 
-namespace Autodesk.Forge.DesignAutomation.Model
+namespace Autodesk.Forge.DesignAutomation.Rsdk.Model
 {
     public class SettingConverter : CustomCreationConverter<ISetting>
     {
