6d44945fb038e24736251c0889fdf51d9e9eab3b
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-01-05 22:13:25 -0300

    Add `GetAllItems` with an extra parameter.
    I create an extra `GetAllItems`  method to work with `pageGetter` function when an extra parameter.
    Like `GetAppBundleVersionsAsync(string id, Page page = null)`.
    ```C#
    await DesignAutomationClient.GetAllItems(DesignAutomationClient.GetAppBundleVersionsAsync, id);
    ```
    Added documentation as well using ChatGPT to generate the `<summary>`.

--- src/Autodesk.Forge.DesignAutomation/ApiClient.cs
+++ src/Autodesk.Forge.DesignAutomation/ApiClient.cs
@@ -130,13 +130,41 @@ namespace Autodesk.Forge.DesignAutomation
             }
         }
 
-        public async Task<List<T>> GetAllItems<T>(Func<string, Task<Page<T>>> pageGetter)
+        /// <summary>
+        /// Asynchronously retrieves a list of all items of type <typeparamref name="T"/> using the specified page getter function.
+        /// </summary>
+        /// <typeparam name="T">The type of the items to retrieve.</typeparam>
+        /// <param name="pageGetter">A function that retrieves a page of items of type <typeparamref name="T"/> given a pagination token.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of all items of type <typeparamref name="T"/>.</returns>
+        public static async Task<List<T>> GetAllItems<T>(Func<string, Task<Page<T>>> pageGetter)
         {
             var ret = new List<T>();
             string paginationToken = null;
             do
             {
                 var resp = await pageGetter(paginationToken);
+                paginationToken = resp.PaginationToken;
+                ret.AddRange(resp.Data);
+            }
+            while (paginationToken != null);
+            return ret;
+        }
+
+        /// <summary>
+        /// Asynchronously retrieves a list of all items of type <typeparamref name="T"/> using the specified page getter function and parameter.
+        /// </summary>
+        /// <typeparam name="T">The type of the items to retrieve.</typeparam>
+        /// <typeparam name="P">The type of the parameter to pass to the page getter function.</typeparam>
+        /// <param name="pageGetter">A function that retrieves a page of items of type <typeparamref name="T"/> given a parameter of type <typeparamref name="P"/> and a pagination token.</param>
+        /// <param name="parameter">The parameter to pass to the page getter function.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of all items of type <typeparamref name="T"/>.</returns>
+        public static async Task<List<T>> GetAllItems<T, P>(Func<P, string, Task<Page<T>>> pageGetter, P parameter)
+        {
+            var ret = new List<T>();
+            string paginationToken = null;
+            do
+            {
+                var resp = await pageGetter(parameter, paginationToken);
                 paginationToken = resp.PaginationToken;
                 ret.AddRange(resp.Data);
             }
